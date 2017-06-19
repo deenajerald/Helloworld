301a7e38878438b3f0247475238d4748c03e1b04
Deena B <23567@Hexaware.com>
2017-06-19 12:20:47 +0530

    again modified

--- Helloworld.js
+++ Helloworld.js
@@ -1,4 +1,4 @@
-var express = require('express')
+/*var express = require('express')
 var app = express();
 
  app.set('port', (process.env.PORT || 5000))
@@ -7,6 +7,22 @@ var app = express();
  app.get('/', function(request, response) {
   response.send('Hello World!')
 })
+*/
+'use strict';
+var apiai = require('./api/ApiInnovation/Helloworld');
+
+var appai = apiai("421ac8ae-4418-4e7c-a822-beb84ec65d20");
+
+var express=require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var portC = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var options = {
+    sessionId: '567yh8'
+};
 
  app.listen(app.get('port'), function() {
    console.log("Node app is running at localhost:" + app.get('port'))
